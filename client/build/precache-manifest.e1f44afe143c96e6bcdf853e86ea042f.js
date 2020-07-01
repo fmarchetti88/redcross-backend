2e2deca63d95d040afaa302d93742705d7f19c5f
f.marchetti <f.marchetti@teamsystem.com>
2020-07-01 16:07:40 +0200

    Added UI BUILD

---
+++ client/build/precache-manifest.e1f44afe143c96e6bcdf853e86ea042f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274d31dffe3aa3d02649983387020f76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce54da5f6ad41064a9ad",
+    "url": "/static/css/2.bdcce013.chunk.css"
+  },
+  {
+    "revision": "388d50cda5feea2b3966",
+    "url": "/static/css/main.9de8dd8c.chunk.css"
+  },
+  {
+    "revision": "ce54da5f6ad41064a9ad",
+    "url": "/static/js/2.9ef2ddef.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.9ef2ddef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388d50cda5feea2b3966",
+    "url": "/static/js/main.60afb454.chunk.js"
+  },
+  {
+    "revision": "3683ff66ea73f75b0169",
+    "url": "/static/js/runtime-main.d5f19d83.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const basicAuth = require('express-basic-auth');
 var cors = require('cors');
 const { User } = require('./sequelize');
 
 const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(cors());
-
 app.use(bodyParser.json());
-
 app.use(require('./routes_no_auth'));
 
 app.use(basicAuth({ authorizer: myAuthorizer, authorizeAsync: true }));
@@ -18,6 +21,12 @@ async function myAuthorizer(username, password, cb) {
 }
 
 app.use(require('./routes'));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
